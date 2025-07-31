28279a6d53511f8d65c7f425083d24ded873092a
ngqkhai <nguyenquangkhai2509@gmail.com>
2025-08-01 02:43:10 +0700

    Fix gateway secret injection

--- api-gateway/lua-plugins/api-gateway-secret/handler.lua
+++ api-gateway/lua-plugins/api-gateway-secret/handler.lua
@@ -7,7 +7,7 @@ function plugin:access(conf)
   kong.log.debug("=== API Gateway Secret Plugin ===")
   
   -- Read the secret from the configuration passed by kong.yml
-  local secret = conf.secret_value
+  local secret = conf.config.secret
   
   kong.service.request.set_header("x-api-gateway-secret", secret)
   kong.log.debug("Set header: x-api-gateway-secret = " .. secret)
