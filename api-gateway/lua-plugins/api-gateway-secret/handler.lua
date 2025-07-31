4b84a36199da1cb870fbed4c6ae41614a8e6d530
ngqkhai <nguyenquangkhai2509@gmail.com>
2025-07-31 16:08:05 +0700

    Modify Dockerfile for deployment

--- api-gateway/lua-plugins/api-gateway-secret/handler.lua
+++ api-gateway/lua-plugins/api-gateway-secret/handler.lua
@@ -1,22 +1,15 @@
 local plugin = {
-  PRIORITY = 1100, -- Higher priority than jwt-claims-headers (1000)
+  PRIORITY = 1100,
   VERSION = "1.0.0",
 }
 
 function plugin:access(conf)
-  kong.log.debug("=== API Gateway Secret Plugin ===")
-  
-  -- Always add API Gateway secret header for service-to-service authentication
-  local secret = conf.api_gateway_secret
-  if not secret or secret == "" then
-    secret = os.getenv("API_GATEWAY_SECRET")
-  end
+  local secret = os.getenv("API_GATEWAY_SECRET")
 
   if secret and secret ~= "" then
     kong.service.request.set_header("x-api-gateway-secret", secret)
-    kong.log.debug("Set header: x-api-gateway-secret = " .. secret)
   else
-    kong.log.warn("API_GATEWAY_SECRET is not configured. x-api-gateway-secret header not set.")
+    kong.log.warn("API_GATEWAY_SECRET is not set. The x-api-gateway-secret header will not be added.")
   end
 end
 
--- api-gateway/lua-plugins/api-gateway-secret/schema.lua
+++ api-gateway/lua-plugins/api-gateway-secret/schema.lua
@@ -1,12 +1,6 @@
 return {
   name = "api-gateway-secret",
   fields = {
-    { config = {
-        type = "record",
-        fields = {
-          { api_gateway_secret = { type = "string", default = "" } },
-        },
-      },
-    },
+    -- No configuration needed for this plugin
   }
 }
